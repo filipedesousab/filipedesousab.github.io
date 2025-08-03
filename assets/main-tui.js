801326fa55837fbe6b5ff84e743fe22ce2fe53fa
Filipe Botelho <filipedesousab@gmail.com>
2025-08-03 19:04:19 -0300

    Add help menu to open welcome modal

--- assets/main-tui.js
+++ assets/main-tui.js
@@ -1,3 +1,82 @@
+class WelcomeModal {
+    static modal = undefined
+    static closeButton = undefined
+    static rememberButton = undefined
+    static afterCloseHooks = []
+
+    static addAfterCloseHook(hook) {
+        this.afterCloseHooks.push(hook)
+    }
+
+    static runAfterCloseHooks() {
+        this.afterCloseHooks.forEach(hook => hook())
+    }
+
+    static load() {
+        this.modal = document.getElementById('welcome')
+        this.closeButton = this.modal.querySelector('#close-button')
+        this.rememberButton = this.modal.querySelector('#remember-button')
+        this.helpButton = document.querySelector('.page-link.help')
+
+        this.closeButton.addEventListener('click', this.close.bind(this))
+        this.rememberButton.addEventListener('click', this.closeAndRemember.bind(this))
+        this.helpButton.addEventListener('click', this.open.bind(this))
+    }
+
+    static loaded() {
+        return !!this.modal
+    }
+
+    static rememberedClosing() {
+        return localStorage.getItem('modalShow') === 'true'
+    }
+
+    static rememberClosing() {
+        localStorage.setItem('modalShow', true)
+    }
+
+    static forgetClosing() {
+        localStorage.setItem('modalShow', false)
+    }
+
+    static open() {
+        this.modal.style.display = 'flex'
+    }
+
+    static modalIsOpen() {
+        return this.modal.style.display === 'flex'
+    }
+
+    static close(event) {
+        event.target.classList.add('active')
+        setTimeout(() => {
+            event.target.classList.remove('active')
+        }, 100)
+        setTimeout(() => {
+            this.modal.style.display = 'none'
+            this.runAfterCloseHooks()
+        }, 200)
+    }
+
+    static closeAndRemember(event) {
+        this.close(event)
+        this.rememberClosing()
+    }
+
+    static navegate(event) {
+        switch (event.key) {
+            case 'ArrowRight':
+                this.rememberButton && this.rememberButton.focus()
+                break
+            case 'Enter':
+                if (document.activeElement == this.rememberButton) break
+            case 'ArrowLeft':
+            default:
+                this.closeButton && this.closeButton.focus()
+        }
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     function focusOnTheWrapper() {
         const menuLink = document.querySelector('.site-nav a:focus')
@@ -6,51 +85,13 @@ document.addEventListener('DOMContentLoaded', function () {
         document.querySelector('.internal-wrapper').focus()
     }
 
-    const modal = document.getElementById('welcome')
-    let closeButton = undefined
-    let rememberButton = undefined
+    WelcomeModal.load()
+    WelcomeModal.addAfterCloseHook(focusOnTheWrapper)
 
-    if (modal && !localStorage.getItem('modalShow')) {
-        closeButton = modal.querySelector('#close-button')
-        rememberButton = modal.querySelector('#remember-button')
-
-        function openModal() {
-            modal.style.display = 'flex'
-        }
-        openModal()
-
-        function closeModal(event) {
-            if (event.target == rememberButton) localStorage.setItem('modalShow', true)
-
-            event.target.classList.add('active')
-            setTimeout(() => {
-                event.target.classList.remove('active')
-            }, 100);
-            setTimeout(() => {
-                modal.style.display = 'none'
-                focusOnTheWrapper()
-            }, 200);
-        }
-
-        closeButton.addEventListener('click', closeModal)
-        rememberButton.addEventListener('click', closeModal)
+    if (WelcomeModal.loaded() && !WelcomeModal.rememberedClosing()) {
+        WelcomeModal.open()
     } else {
         focusOnTheWrapper()
-    }
-
-    function focusOnTheOpenModal(event) {
-        if (modal && window.getComputedStyle(modal).display === 'flex') {
-            switch (event.key) {
-                case 'Enter':
-                    break;
-                case 'ArrowRight':
-                    rememberButton && rememberButton.focus()
-                    break
-                case 'ArrowLeft':
-                default:
-                    closeButton && closeButton.focus()
-            }
-        }
     }
 
     function animateThePressedButton(event) {
@@ -60,22 +101,22 @@ document.addEventListener('DOMContentLoaded', function () {
             currentLink.classList.add('active')
             setTimeout(() => {
                 currentLink.classList.remove('active')
-            }, 100);
+            }, 100)
             setTimeout(() => {
                 if (currentLink.href) window.location.href = currentLink.href
-            }, 200);
+            }, 200)
         }
     }
 
     function getVisibleLinks() {
-        const internalWrapper = document.querySelector('.internal-wrapper');
-        const allLinks = Array.from(document.querySelectorAll('.internal-wrapper a:not([hidden])'));
-        const internalWrapperRect = internalWrapper.getBoundingClientRect();
+        const internalWrapper = document.querySelector('.internal-wrapper')
+        const allLinks = Array.from(document.querySelectorAll('.internal-wrapper a:not([hidden])'))
+        const internalWrapperRect = internalWrapper.getBoundingClientRect()
 
         return allLinks.filter(link => {
-            const linkRect = link.getBoundingClientRect();
-            return linkRect.top < internalWrapperRect.bottom && linkRect.bottom > internalWrapperRect.top;
-        });
+            const linkRect = link.getBoundingClientRect()
+            return linkRect.top < internalWrapperRect.bottom && linkRect.bottom > internalWrapperRect.top
+        })
     }
 
     function selectLink(direction) {
@@ -112,7 +153,10 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     document.addEventListener('keydown', function (event) {
-        focusOnTheOpenModal(event)
+        if (WelcomeModal.loaded() && WelcomeModal.modalIsOpen()) {
+            WelcomeModal.navegate(event)
+            return
+        }
 
         if (event.altKey) {
             switch (event.key) {
@@ -127,6 +171,9 @@ document.addEventListener('DOMContentLoaded', function () {
                 case 's':
                 case 'S':
                     document.querySelector('.page-link.s').focus()
+                    break
+                case '?':
+                    document.querySelector('.page-link.help').focus()
                     break
                 default:
                     event.preventDefault()
@@ -153,5 +200,5 @@ document.addEventListener('DOMContentLoaded', function () {
                     selectMenuLink(-1)
             }
         }
-    });
-});
+    })
+})
